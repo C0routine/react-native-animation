d4b576c2426783dea4d353dbcc99836f19add330
Coroutine <whdrjsco@gmail.com>
2022-04-12 22:22:03 +0900

    reanimated upgrade

--- src/Reanimated/ColorPicker.js
+++ src/Reanimated/ColorPicker.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Dimensions} from 'react-native';
+import ColorPickerComponent from '../components/ColorPickerComponent';
 import GoBackView from '../GoBackView';
 
-const COLORS = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'indigo',
-  'purple',
-  'black',
-  'white',
-];
+const COLORS = [];
+// for(let i=0; )
 
 const ColorPicker = ({navigation}) => {
+  const isFocused = useIsFocused();
+  const [colors, setColors] = useState([0]);
+  const {width} = Dimensions.get('window');
+
+  const SettingGradient = () => {
+    let temp = [];
+    for (let i = 0; i < 256; i++) {
+      temp.push(i);
+    }
+    setColors(temp);
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      SettingGradient();
+    }
+  }, []);
 
   return (
     <View style={se.all}>
       <GoBackView navigation={navigation} />
+
+      <View style={{flex: 3, backgroundColor: 'white'}}>
+        <View style={{width:100, height:100, backgroundColor:'rgba(255,0,200,0.5)'}}/>
+
+      </View>
       <View style={{flex: 1, backgroundColor: 'white'}}>
-          <ColorPicker colors={COLORS}/>
+        {/* <ColorPickerComponent colors={COLORS} /> */}
+        {colors.map((co, index) => {
+          
+          return <View key={index} style={{flex:1, backgroundColor: `rgba(255,255,${index},1)`}} />;
+        })}
       </View>
     </View>
   );
--- src/components/ColorPickerComponent.js
+++ src/components/ColorPickerComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 
 const ColorPickerComponent = ({colors}) => {
+  console.log(colors);
   return (
-      <LinearGradient colors={colors} style={{height:100, width:'90%'}}/>
+    <View>
+
+    </View>
   );
 };
 
